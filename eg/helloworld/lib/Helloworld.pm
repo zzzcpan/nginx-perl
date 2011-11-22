4584879d83967b85b590007479522681088da943
Alexandr Gomoliako <zzz@zzz.org.ua>
2011-11-22 03:35:38 +0200

    eg/helloworld

---
+++ eg/helloworld/lib/Helloworld.pm
@@ -0,0 +1,68 @@
+package Helloworld;
+
+use strict;
+use warnings;
+no  warnings 'uninitialized';
+
+use Sys::Hostname;
+
+use Nginx;
+use Nginx::Util;
+
+our $NAME = 'unnamed';
+
+
+sub init_worker {
+
+    my $hostname = hostname;
+
+    warn << "    END";
+
+
+    Helloworld is running on *:55555
+
+        http://127.0.0.1:55555/
+        http://$hostname:55555/
+
+
+    END
+}
+
+
+sub handler {
+    my ($r) = @_;
+
+    return OK
+        if $r->has_request_body ( \&handler_post );
+
+    return &handler_post;
+}
+
+
+sub handler_post {
+    my ($r) = @_;
+
+    $r->main_count_inc; 
+
+
+    my $buf = "Hello from $NAME\n";
+
+    $r->send_http_header('text/html; charset=UTF-8');
+
+    $r->header_out('Cache-Control',  'no-cache');
+    $r->header_out('Pragma',         'no-cache');
+    $r->header_out('Content-Length', length($buf));
+
+    $r->print($buf)
+        unless $r->header_only;
+
+    $r->send_special(NGX_HTTP_LAST);
+    $r->finalize_request(NGX_OK);
+
+
+    return NGX_DONE;
+}
+
+
+1;
+
